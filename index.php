891b860d5e90a8d68e3007d255452e2ba869085c
Augustin Nalzaro <54712945+DeathThreats@users.noreply.github.com>
2020-08-13 22:12:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -18,15 +18,15 @@ if(isset($_POST['submit'])){
 	$mail->isSMTP();                                            // Send using SMTP
 	$mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
 	$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-	$mail->Username   = 'vludenburg@gmail.com';                 // SMTP username
-	$mail->Password   = '82&@ueht826Hge';                        // SMTP password
+	$mail->Username   = 'YOUREMAIL@MAILER.COM';                 // SMTP username
+	$mail->Password   = 'SECRETKEY';                        // SMTP password
 	$mail->SMTPSecure = 'tls';                                  // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 	$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 	//Recipients
 	$mail->setFrom($_POST['email'], $_POST['name']);
 
-	$mail->addAddress('vludenburg@gmail.com');                  // Add a recipient, In this case your email address 
+	$mail->addAddress('YOUREMAIL@MAILER.COM');                  // Add a recipient, In this case your email address 
 
 	// Content
 	$mail->isHTML(true);                                        // Set email format to HTML
